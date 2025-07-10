b2019b0e97fd95ddb510a28f985d124da346c821
Elder <elder.lopez@elektra.com.mx>
2025-07-10 16:35:43 -0600

    se agrega carrito de compras

--- app/modules/login/login.ts
+++ app/modules/login/login.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { Auth } from '../../core/services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
   constructor(private auth: Auth) {}
 
+  public username: string = '';
+  public password: string = '';
+
   login() {
+    console.log(this.username, this.password);
+    
     this.auth.login('admin', '123456').subscribe((response) => {
       console.log(response);
     });
