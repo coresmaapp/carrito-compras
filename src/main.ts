90e36a9f160dd43789b5b0b32d01d5eb7a1b862b
Elder <elder.lopez@elektra.com.mx>
2025-07-17 21:07:34 -0600

    init

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import { environment } from './environments/environment';
+console.log('Entorno:', environment);
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
